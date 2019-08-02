1230d1851aeece8dab6835fe3ea49d0d7ec38ea2
lvdou <jack.wang@alltennis.cn>
2019-08-02 16:56:15 +0800

    新文件：   run_build.sh

---
+++ run_build.sh
@@ -0,0 +1,6 @@
+yarn build
+
+cp -rf types/ ~/softwares/libs/js/mand-mobile-component/
+cp -rf lib/ ~/softwares/libs/js/mand-mobile-component/
+cp -rf lib-vw/ ~/softwares/libs/js/mand-mobile-component/
+cp -rf components/ ~/softwares/libs/js/mand-mobile-component/
